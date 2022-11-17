f93f1908f140b72ceda36ec2d0fda4106b9c0242
zman811 <sunburnzman@gmail.com>
2022-11-17 14:01:29 -0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <nav>
+        <ul>
+          <li>Test</li>
+        </ul>
+        <ul>
+          <li>test</li>
+        </ul>
+      </nav>
+    Hello world
+
+    </div>
+  )
+}
